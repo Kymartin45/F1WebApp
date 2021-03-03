987bd486ee397071dee332a57e33ced5a4d1d2c9
Kyle Martin <kymartin@csumb.edu>
2021-03-03 11:25:57 -0800

    Track list for 2021 schedule

---
+++ tracks.py
@@ -0,0 +1,27 @@
+# Track names for 2021 schedule
+# May change due to some track additions/removal (Approval by the FIA)
+# Source: https://www.formula1.com/en/racing/2021.html
+
+trackNames =  [
+    'Bahrain International Circuit',
+    'Autodromo Enzo e Dino Ferrari',
+    'Circuit de Barcelona - Catalunya',
+    'Circuit de Monaco',
+    'Baku City Circuit',
+    'Circuit Gilles-Villeneuve',
+    'Circuit Paul Ricard',
+    'Red Bull Ring',
+    'Silverstone Circuit',
+    'Hungaroring',
+    'Cirucit de Spa-Francorchamps',
+    'Circuit Zandvoort',
+    'Autodromo Nazionale Monza',
+    'Sochi Autodrom',
+    'Marina Bay Street Circuit',
+    'Suzuka International Racing Course',
+    'Circuit of the Americas',
+    'Autodromo Hermanos Rodriguez',
+    'Autodromo Jose Carlos Pace',
+    'Melbourne Grand Prix Circuit',
+    'Yas Marina Circuit'
+]
