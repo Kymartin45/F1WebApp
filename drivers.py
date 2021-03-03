66079a7a8389ce9eae1b67f2fb453d8509cc328e
Kyle Martin <kymartin@csumb.edu>
2021-03-03 11:51:26 -0800

    Driver names added

---
+++ drivers.py
@@ -0,0 +1,26 @@
+from pprint import pprint
+
+driverNames = [
+    'Kimi Raikkonen', 
+    'Antonion Giovinazzi',
+    'Pierre Gasly',
+    'Yuki Tsunoda',
+    'Esteban Ocon',
+    'Fernando Alonso',
+    'Sebastian Vettel',
+    'Lance Stroll',
+    'Charles Leclerc',
+    'Carlos Sainz Jr.',
+    'Nikita Mazepin',
+    'Mick Schumacher',
+    'Daniel Ricciardo',
+    'Lando Norris',
+    'Valtteri Bottas',
+    'Lewis Hamilton',
+    'Max Verstappen',
+    'Sergio Perez',
+    'Nicholas Latifi',
+    'George Russell'
+]
+
+pprint(driverNames)
