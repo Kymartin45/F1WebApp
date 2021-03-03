5ae431364f67af3be5464d138f7b27e04f342cc5
Kyle Martin <kymartin@csumb.edu>
2021-03-03 11:53:07 -0800

    Sidebar additions

--- f1App.py
+++ f1App.py
@@ -1,11 +1,28 @@
+from urllib.parse import uses_relative
 import streamlit as st
 import numpy as np 
 import pandas as pd
-import base64
+import base64 
 import matplotlib.pyplot as plt
 import seaborn as sns 
 import numpy as np 
+import tracks
+
 
 st.title('F1 Schedule and Driver Statistics')
-st.markdown("Author: Kyle Martin")
+st.markdown("**Author: Kyle Martin**")
+st.subheader('Web scraping 2021 F1 track schedule and driver statistics')
 
+st.sidebar.header('Track Information')
+trackName = st.sidebar.selectbox('Tracks', tracks.trackNames)
+
+def getTrack(trackName):
+    url = "https://www.formula1.com/en/racing/2021/" + str(trackName) + ".html"
+    html = pd.read_html(url, header=0)
+    df = html[0]
+    raw = df.drop(df[df.Age == 'Age'].index)
+    raw = raw.fillna(0)
+    trackStats = raw.drop(['Rk'], axis=1)
+    return trackStats
+trackStats = getTrack(trackName)
+    
