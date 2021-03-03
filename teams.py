bdccf4dfd31c7dbce58e5ecc5a5104f94e26a278
Kyle Martin <kymartin@csumb.edu>
2021-03-03 14:44:08 -0800

    Team names added

---
+++ teams.py
@@ -0,0 +1,12 @@
+teamName = [
+    'Mercedes',
+    'Alpine',
+    'Haas F1 Team',
+    'McLaren',
+    'Alfa Romeo Racing',
+    'Red Bull Racing',
+    'Aston Martin',
+    'Ferrari',
+    'AlphaTauri',
+    'Williams'
+]
