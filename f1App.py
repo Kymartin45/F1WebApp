c7ae184849e20322232f40f391760288e7bdfcf4
Kyle Martin <kymartin@csumb.edu>
2021-03-03 10:46:06 -0800

    First commit

---
+++ f1App.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np 
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns 
+import numpy as np 
+
+st.title('F1 Schedule and Driver Statistics')
+st.markdown("Author: Kyle Martin")
+
